fb5b70c013a65084aabcd5b08a47ad67e3449b29
sriharsha1235 <sriharsha@azrisolutions.com>
2016-04-25 15:39:10 +0530

    printing the permissions by grouping the modules

--- src/Form/RolesCompareForm.php
+++ src/Form/RolesCompareForm.php
@@ -31,7 +31,8 @@ class RolesCompareForm extends FormBase {
    * @param \Drupal\Core\Extension\ModuleHandlerInterface
    *   The module handler.
    */
-  public function __construct(PermissionHandlerInterface $permission_handler, RoleStorageInterface $role_storage, ModuleHandlerInterface $module_handler) {
+  public function __construct(PermissionHandlerInterface $permission_handler, RoleStorageInterface $role_storage, ModuleHandlerInterface $module_handler
+  ) {
     $this->permissionHandler = $permission_handler;
     $this->roleStorage = $role_storage;
     $this->moduleHandler = $module_handler;
@@ -57,6 +58,16 @@ class RolesCompareForm extends FormBase {
   }
 
   /**
+   * Gets the roles to display in this form.
+   *
+   * @return \Drupal\user\RoleInterface[]
+   *   An array of role objects.
+   */
+  protected function getRoleLabel($role_name = '') {
+    return $this->roleStorage->load($role_name)->label();
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getFormId() {
@@ -69,14 +80,14 @@ class RolesCompareForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['select_role1'] = array(
       '#type' => 'select',
-      '#title' => $this->t('Select Role'),
+      '#title' => $this->t('Role'),
       '#options' => user_role_names(),
       '#required' => TRUE,
     );
     $form['select_role2'] = array(
       '#type' => 'select',
       '#required' => TRUE,
-      '#title' => $this->t('Select Role'),
+      '#title' => $this->t('Role'),
       '#options' => user_role_names(),
     );
     $form['actions'] = array('#type' => 'actions');
@@ -104,21 +115,20 @@ class RolesCompareForm extends FormBase {
     $role1_permissions = $this->getRolePermission($role1);
     $role2_permissions = $this->getRolePermission($role2);
 
-    $role_diff = array_merge(array_diff($role2_permissions, $role1_permissions), array_diff($role1_permissions, $role2_permissions));
+    $role_diff = array_merge(
+        array_diff($role2_permissions, $role1_permissions), array_diff($role1_permissions, $role2_permissions)
+    );
     if (!empty($role_diff)) {
       $permissions = $this->permissionHandler->getPermissions();
       $form['role_diff'] = array(
         '#type' => 'table',
-        '#header' => array('Permission', $role1, $role2),
+        '#header' => array('Module', 'Permission', $this->getRoleLabel($role1), $this->getRoleLabel($role2)),
       );
+
       for ($i = 0; $i < count($role_diff); $i++) {
-        $form['role_diff'][$i]['permission'] = array(
-          '#type' => 'inline_template',
-          '#template' => '<div class="permission"><span class="module_name">' . $this->moduleHandler->getName($permissions[$role_diff[$i]]['provider']) . '<br></span><span class="title">{{ title }}</span></div>',
-          '#context' => array(
-            'title' => $permissions[$role_diff[$i]]['title']
-          ),
-        );
+        $module_proiver = $permissions[$role_diff[$i]]['provider'];
+        $permission_module_list[$module_proiver][$i]['module'] = $this->moduleHandler->getName($permissions[$role_diff[$i]]['provider']);
+        $permission_module_list[$module_proiver][$i]['permission'] = $permissions[$role_diff[$i]]['title'];
         $role1_has = $role2_has = 'No';
         if (in_array($role_diff[$i], $role1_permissions)) {
           $role1_has = 'Yes';
@@ -126,18 +136,34 @@ class RolesCompareForm extends FormBase {
         if (in_array($role_diff[$i], $role2_permissions)) {
           $role2_has = 'Yes';
         }
-        $form['role_diff'][$i][$role1] = array(
-          '#markup' => $role1_has,
-        );
+        $permission_module_list[$module_proiver][$i]['role1'] = $role1_has;
+        $permission_module_list[$module_proiver][$i]['role2'] = $role2_has;
+      }
+      foreach ($permission_module_list as $provider => $module_list) {
+        foreach ($module_list as $key => $permission_list) {
+          $form['role_diff'][$key]['module'] = array(
+            '#markup' => $permission_list['module']
+          );
+          $form['role_diff'][$key]['permission'] = array(
+            '#type' => 'inline_template',
+            '#template' => '<div class="permission"><span class="title">{{ title }}</span></div>',
+            '#context' => array(
+              'title' => $permission_list['permission']
+            ),
+          );
+          $form['role_diff'][$key][$role1] = array(
+            '#markup' => $permission_list['role1'],
+          );
 
-        $form['role_diff'][$i][$role2] = array(
-          '#markup' => $role2_has
-        );
+          $form['role_diff'][$key][$role2] = array(
+            '#markup' => $permission_list['role2']
+          );
+        }
       }
     }
     else {
       $form['no_diff'] = array(
-        '#markup' => 'Both the selected roles has same permissions'
+        '#markup' => 'Both the selected roles has similar permissions'
       );
     }
   }
